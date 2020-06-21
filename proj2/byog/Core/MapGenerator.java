f97e688be53e6475ed383c1bc8752a831717fff8
Wyatt <wyatt.wang@outlook.com>
2020-06-21 09:39:13 +0800

    Create MapGenerator.java

---
+++ proj2/byog/Core/MapGenerator.java
@@ -0,0 +1,14 @@
+package byog.Core;
+
+import byog.TileEngine.*;
+import java.util.Random;
+
+public class MapGenerator {
+
+    public static TETile[][] createWorld(TETile[][] world, Random random) {
+
+        return world;
+
+    }
+
+}
